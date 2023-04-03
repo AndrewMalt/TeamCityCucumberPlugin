b61b98a17ce0d6721e3f15a28cd9a3026cf83a74
Andrew Malt <naotanoyou@gmail.com>
2023-04-03 13:46:52 +0300

    jarjar

--- src/main/java/steps/StepDefinitions.java
+++ src/main/java/steps/StepDefinitions.java
@@ -4,6 +4,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import org.junit.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.TreeSet;
+
 public class StepDefinitions {
 
   @Given("say {string}")
@@ -16,4 +22,13 @@ public class StepDefinitions {
   public void sendBooleanVariable(String variable) {
     Assert.assertTrue("test failed", Boolean.parseBoolean(variable));
   }
+
+  @And("read text")
+  public void readText() throws IOException {
+    Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/text.txt");
+    String str = new String(Files.readAllBytes(path));
+    System.out.println("------------->> > > " + str);
+  }
 }
+
+
